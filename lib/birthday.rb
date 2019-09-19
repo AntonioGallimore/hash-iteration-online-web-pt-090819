ff1682ab78be08761d11e4e5ed9179148631cb88
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-09-19 15:32:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ def happy_birthday(birthday_kids)
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    age =< 12
+    age => 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
  end
